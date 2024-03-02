a2841936ee7e6e8252f0627d771f5f504271b948
islam7676 <islam7676@gmail.com>
2024-03-02 13:49:14 +0200

    update FRM_Brands

--- Arabology_ERP/PL/Inventory/FRM_Brands.Designer.cs
+++ Arabology_ERP/PL/Inventory/FRM_Brands.Designer.cs
@@ -29,24 +29,24 @@ namespace Arabology_ERP.PL.Inventory
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.brID1 = new TestNew.BrID();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.BrandId = new TestNew.BrID();
             this.search1 = new TestNew.UsCo.Search();
-            this.brNameA1 = new TestNew.BrNameA();
-            this.brNameE1 = new TestNew.UsCo.BrNameE();
+            this.BrandNameA = new TestNew.BrNameA();
+            this.BrandNameE = new TestNew.UsCo.BrNameE();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // brID1
+            // BrandId
             // 
-            this.brID1.Location = new System.Drawing.Point(697, 173);
-            this.brID1.Name = "brID1";
-            this.brID1.Size = new System.Drawing.Size(185, 41);
-            this.brID1.TabIndex = 1;
+            this.BrandId.Location = new System.Drawing.Point(697, 173);
+            this.BrandId.Name = "BrandId";
+            this.BrandId.Size = new System.Drawing.Size(185, 41);
+            this.BrandId.TabIndex = 1;
             // 
             // search1
             // 
@@ -60,21 +60,21 @@ namespace Arabology_ERP.PL.Inventory
             this.search1.Size = new System.Drawing.Size(450, 91);
             this.search1.TabIndex = 2;
             // 
-            // brNameA1
+            // BrandNameA
             // 
-            this.brNameA1.Location = new System.Drawing.Point(461, 234);
-            this.brNameA1.Name = "brNameA1";
-            this.brNameA1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.brNameA1.Size = new System.Drawing.Size(449, 43);
-            this.brNameA1.TabIndex = 3;
-            this.brNameA1.Load += new System.EventHandler(this.brNameA1_Load);
+            this.BrandNameA.Location = new System.Drawing.Point(461, 234);
+            this.BrandNameA.Name = "BrandNameA";
+            this.BrandNameA.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.BrandNameA.Size = new System.Drawing.Size(449, 43);
+            this.BrandNameA.TabIndex = 3;
+            this.BrandNameA.Load += new System.EventHandler(this.brNameA1_Load);
             // 
-            // brNameE1
+            // BrandNameE
             // 
-            this.brNameE1.Location = new System.Drawing.Point(461, 286);
-            this.brNameE1.Name = "brNameE1";
-            this.brNameE1.Size = new System.Drawing.Size(455, 44);
-            this.brNameE1.TabIndex = 4;
+            this.BrandNameE.Location = new System.Drawing.Point(461, 286);
+            this.BrandNameE.Name = "BrandNameE";
+            this.BrandNameE.Size = new System.Drawing.Size(455, 44);
+            this.BrandNameE.TabIndex = 4;
             // 
             // label1
             // 
@@ -88,33 +88,33 @@ namespace Arabology_ERP.PL.Inventory
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.Location = new System.Drawing.Point(12, 58);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Size = new System.Drawing.Size(443, 454);
             this.dataGridView1.TabIndex = 6;
             // 
@@ -126,10 +126,10 @@ namespace Arabology_ERP.PL.Inventory
             this.ClientSize = new System.Drawing.Size(919, 520);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.brNameE1);
-            this.Controls.Add(this.brNameA1);
+            this.Controls.Add(this.BrandNameE);
+            this.Controls.Add(this.BrandNameA);
             this.Controls.Add(this.search1);
-            this.Controls.Add(this.brID1);
+            this.Controls.Add(this.BrandId);
             this.Name = "FRM_Brands";
             this.Text = "FRM_Brands";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -140,10 +140,10 @@ namespace Arabology_ERP.PL.Inventory
 
         #endregion
 
-        private TestNew.BrID brID1;
+        private TestNew.BrID BrandId;
         private TestNew.UsCo.Search search1;
-        private TestNew.BrNameA brNameA1;
-        private TestNew.UsCo.BrNameE brNameE1;
+        private TestNew.BrNameA BrandNameA;
+        private TestNew.UsCo.BrNameE BrandNameE;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
